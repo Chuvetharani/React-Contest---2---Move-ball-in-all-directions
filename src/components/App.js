72157ea73d4d39b1d7d7ec38017d1bc32f2638c2
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-11-01 12:36:53 +0530

    ballinalldirection

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,75 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
 
+  const handleBallMovement = (event) => {
+    if (
+      event.keyCode !== 37 &&
+      event.keyCode !== 38 &&
+      event.keyCode !== 39 &&
+      event.keyCode !== 40
+    )
+      return;
+
+    let leftPos = Number(ballPosition.left.split("px")[0]);
+    let topPos = Number(ballPosition.top.split("px")[0]);
+
+    switch (event.keyCode) {
+      case 37:
+        leftPos -= 5;
+        break;
+      case 38:
+        topPos -= 5;
+        break;
+      case 39:
+        leftPos += 5;
+        break;
+      case 40:
+        topPos += 5;
+        break;
+      default:
+        break;
+    }
+
+    const newPos = {
+      left: `${leftPos}px`,
+      top: `${topPos}px`
+    };
+    setBallPosition(newPos);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleBallMovement);
+
+    return () => {
+      document.removeEventListener("keydown", handleBallMovement);
+    };
+  });
+
+  const reset = () => {
+    setRenderBall(false);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      );
+  };
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
